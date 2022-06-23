908e45df7fd598ecffa1bb5a303395eb8f7f46bf
yushang <2797654843@qq.com>
2022-06-23 19:14:23 +0800

    修正

--- db/index.js
+++ db/index.js
@@ -4,9 +4,9 @@ const mysql = require('mysql')
 // 创建数据库连接对象
 const db = mysql.createPool({
   host: '127.0.0.1',
-  user: 'root',
-  password: 'admin123',
-  database: 'my_db_o1',
+  user: '数据库用户名',
+  password: '数据库密码',
+  database: '对应数据库',
 })
 
 module.exports = db
